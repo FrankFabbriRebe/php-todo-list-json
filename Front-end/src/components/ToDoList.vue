f62a91c2e5bc31ccfce1186f7f8fcc28bbbce1ad
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-25 17:10:03 +0100

    call API

--- Front-end/src/components/ToDoList.vue
+++ Front-end/src/components/ToDoList.vue
@@ -1,13 +1,37 @@
 <script>
 
 import axios from 'axios';
+import { onMounted } from 'vue';
 
 export default {
     name: "ToDoList",
+
+    data() {
+        toDoList: []
+    },
+
+    mounted() {
+
+        const t = this;
+        axios.get('http://localhost/php-todo-list-json/Back-end/toDoListAPI.php')
+            .then(res => {
+                // console.log("data: " + JSON.stringify(res.data));
+                t.toDoList = res.data;
+                console.log(t.toDoList);
+            }).catch(err => console.error(err));
+
+    }
+
 }
+
+
 
 </script>
 
 <template>
-    <h1>Test</h1>
+    <ul>
+        <li v-for="(toDo, index) in toDoList" :key="index">
+            {{ toDo.task }}
+        </li>
+    </ul>
 </template>
